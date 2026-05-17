259e4b07d0bfd60766970b98a90f802494a08558
chae-ring <149919388+chae-ring@users.noreply.github.com>
2026-05-18 03:12:39 +0900

    feat: add findByNicknameContaining method

--- src/main/java/com/danwoog/todo/repository/UserRepository.java
+++ src/main/java/com/danwoog/todo/repository/UserRepository.java
@@ -3,11 +3,12 @@ package com.danwoog.todo.repository;
 import com.danwoog.todo.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByLoginId(String loginId);
-
+    List<User> findByNicknameContaining(String keyword);
     Optional<User> findByLoginId(String loginId);
 }
