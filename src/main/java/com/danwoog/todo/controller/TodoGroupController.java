689bbaabc9bdd717af5fafc5398474e12a9dd6c5
GuriGuri22 <studybeen22@gmail.com>
2026-05-18 16:20:40 +0900

    fix: add authentication to todo group controller

--- src/main/java/com/danwoog/todo/controller/TodoGroupController.java
+++ src/main/java/com/danwoog/todo/controller/TodoGroupController.java
@@ -3,9 +3,15 @@ package com.danwoog.todo.controller;
 import com.danwoog.todo.dto.todogroup.TodoGroupCreateRequest;
 import com.danwoog.todo.dto.todogroup.TodoGroupCreateResponse;
 import com.danwoog.todo.service.TodoGroupService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Todo Group API", description = "공동 할 일 그룹 API")
+@SecurityRequirement(name = "Bearer Authentication")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/todo-groups")
@@ -13,12 +19,13 @@ public class TodoGroupController {
 
     private final TodoGroupService todoGroupService;
 
+    @Operation(summary = "공동 할 일 그룹 생성 및 친구 초대", description = "공동 할 일 그룹을 생성하고 친구를 초대합니다.")
     @PostMapping
     public TodoGroupCreateResponse createGroup(
+            Authentication authentication,
             @RequestBody TodoGroupCreateRequest request
     ) {
-        Long loginUserId = 1L;
-
-        return todoGroupService.createGroup(loginUserId, request);
+        Long userId = (Long) authentication.getPrincipal();
+        return todoGroupService.createGroup(userId, request);
     }
 }
