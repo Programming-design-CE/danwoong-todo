8953b37077b4daa86f054d0d75a9b9b93f8bb94d
Hyunsuk <thdgustjr99@gmail.com>
2026-06-04 21:41:48 +0900

    fix main hover

--- src/main/java/com/danwoog/todo/global/ShopCatalogInitializer.java
+++ src/main/java/com/danwoog/todo/global/ShopCatalogInitializer.java
@@ -28,7 +28,7 @@ public class ShopCatalogInitializer implements CommandLineRunner {
                 new ShopSeedItem("노트북", "ACCESSORY", "/assets/shop_accessory_notebook.svg", 150),
                 new ShopSeedItem("아톰 신발", "ACCESSORY", "/assets/shop_atomshoes.svg", 200),
                 new ShopSeedItem("창", "ACCESSORY", "/assets/shop_spear.svg", 250),
-                new ShopSeedItem("손 노트북", "HAND_ACCESSORY", "/assets/shop_accessory_notebook.svg", 160)
+                new ShopSeedItem("손 노트북", "HAND_ACCESSORY", "/assets/shop_accessory_notebook.svg", 160),
                 new ShopSeedItem("상처", "FACE", "/assets/shop_hurt.svg", 50),
                 new ShopSeedItem("트로피", "ACCESSORY", "/assets/shop_trophy.svg", 180)
         );
